289af542f58f82c5ca531cb49e17805c0897a086
pjs33 <pjs33@email.arizona.edu>
2016-01-23 01:45:44 -0700

    Updated database creation scripts

--- sql/lobby_tables.sql
+++ sql/lobby_tables.sql
@@ -1,32 +1,39 @@
-CREATE TABLE issues (
-	issueID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+DROP TABLE IF EXISTS donations;
+DROP TABLE IF EXISTS issues;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+	user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	realname VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	is_lobbyist BOOLEAN NOT NULL,
+	password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE issues 
+(
+	issue_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	goal_amount INT NOT NULL,
 	current_amount INT NOT NULL,
 	donors INT NOT NULL,
 	picture VARCHAR(255) NOT NULL,
-	fk_lobbyistID VARCHAR(255) NOT NULL,
+	fk_lobbyist_id INT NOT NULL,
 	
-	FOREIGN KEY (fk_lobbyistID) REFERENCES users(userID)
+	FOREIGN KEY (fk_lobbyist_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE donation
+CREATE TABLE donations
 (
-	fk_userID INT NOT NULL,
-	fk_issueID INT NOT NULL,
+	fk_user_id INT NOT NULL,
+	fk_issue_id INT NOT NULL,
 	money_amount INT NOT NULL,
-	FOREIGN KEY (fk_userID) REFERENCES users(userID),
-	FOREIGN KEY (fk_issues) REFERENCES issues(issueID),
+	FOREIGN KEY (fk_user_id) REFERENCES users(user_id),
+	FOREIGN KEY (fk_issue_id) REFERENCES issues(issue_id),
 	
-	CONSTRAINT donation PRIMARY KEY (fk_userID, fk_issueID)
+	CONSTRAINT donation PRIMARY KEY (fk_user_id, fk_issue_id)
 );
 
-CREATE TABLE users
-(
-	userID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	realname VARCHAR(255) NOT NULL,
-	username VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	isLobbyist BOOLEAN NOT NULL;
-	password VARCHAR(255) NOT NULL,
-);
+
