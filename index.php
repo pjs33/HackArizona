9c095c71cb7b9c76e2de024b28c821aa71c22606
mlewelling <Michael@dhcp-10-134-199-212.uawifi.arizona.edu>
2016-01-22 23:59:34 -0700

    Changed Slider format, added bodyContent div

--- index.php
+++ index.php
@@ -21,70 +21,71 @@
     ?>
 
 
-<!-- START CAROUSEL -->
+<div id="bodyContent">
 
-<div class="container">
-  <br>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
+  <!-- START CAROUSEL -->
+  <div id="sliderContainer">
+    <br>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 900px;">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <a href="https://www.google.com/" target="_blank"><img src="./css/images/img1.jpg" alt="img1" width="460" height="345"></a>
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <a href="https://www.google.com/" target="_blank"><img src="./css/images/img1.jpg" alt="img1" width="460" height="345"></a>
+        </div>
+
+        <div class="item">
+          <img src="./css/images/img2.jpg" alt="img2" width="460" height="345">
+        </div>
+      
+        <div class="item">
+          <img src="./css/images/img3.jpg" alt="img3" width="460" height="345">
+        </div>
+
+        <div class="item">
+          <img src="./css/images/img4.jpg" alt="img4" width="460" height="345">
+        </div>
       </div>
 
-      <div class="item">
-        <img src="./css/images/img2.jpg" alt="img2" width="460" height="345">
-      </div>
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+
+  <!-- END CAROUSEL -->
+
+  <!-- START COLUMN BUTTONS -->
+
+  <div id="buttonContainer">
     
-      <div class="item">
-        <img src="./css/images/img3.jpg" alt="img3" width="460" height="345">
+    <div class="row">
+      <div class="col-sm-4">
+  	    <a href="#" type="button" class="btn btn-danger btn-block"><span class="glyphicon glyphicon-edit"></span> Local</a>
       </div>
-
-      <div class="item">
-        <img src="./css/images/img4.jpg" alt="img4" width="460" height="345">
+      <div class="col-sm-4">
+         <a href="#" type="button" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-edit"></span> State</a>		
       </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
-<!-- END CAROUSEL -->
-
-<!-- START COLUMN BUTTONS -->
-
-<div class="container">
-  
-  <div class="row">
-    <div class="col-sm-4">
-	    <a href="#" type="button" class="btn btn-danger btn-block"><span class="glyphicon glyphicon-edit"></span> Local</a>
-    </div>
-    <div class="col-sm-4">
-       <a href="#" type="button" class="btn btn-primary btn-block"><span class="glyphicon glyphicon-edit"></span> State</a>		
-    </div>
-    <div class="col-sm-4">
-      <a href="#" type="button" class="btn btn-danger btn-block"><span class="glyphicon glyphicon-edit"></span> National</a>
+      <div class="col-sm-4">
+        <a href="#" type="button" class="btn btn-danger btn-block"><span class="glyphicon glyphicon-edit"></span> National</a>
+      </div>
     </div>
   </div>
+  <!-- END COLUMN BUTTONS -->
 </div>
-
-<!-- END COLUMN BUTTONS -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
