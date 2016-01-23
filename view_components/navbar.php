1deb6e3b364ab82b344d902c2223c21416e48ed4
pjs33 <pjs33@email.arizona.edu>
2016-01-22 23:53:52 -0700

    Made names for divs in navbar.php nicer

--- view_components/navbar.php
+++ view_components/navbar.php
@@ -1,48 +1,5 @@
 <style type="text/css">
-#nav-bar {
-    margin-right: auto;
-    margin-left: auto;
-}
-
-.nav-bar-link {
-    color: #dcdcdc;
-    font-size: 16px;
-    line-height: 22px;
-    font-weight: 700;
-    letter-spacing: 1px;
-    text-decoration: none;
-    cursor: auto;
-    text-transform: uppercase;
-    height: 40px;
-    padding: 8px;
-    margin: 0px;
-    margin-left: 4px;
-    padding-left: 35px;
-    padding-right: 35px;
-}
-
-.nav-bar-link:hover {
-    background: #666666;
-    text-shadow: none;
-    color: white;
-    text-decoration: none;
-}
-
-#nav-bar-container {
-    width: 100%;
-    height: 45px;
-    background: rgba(30,30,30,0.8);
-    text-align: center;
-    padding-top: 3px;
-}
-
-.nav-link-separator {
-    border-right: 1px solid white;
-    display: inline;
-    margin: 2px;
-}
-
-.univ-header-bg {
+#header-container {
   width: 100%;
   height: 100px;
   background: #0c234b;
@@ -50,29 +7,71 @@
   position: relative;
 }
 
-.univ-header {
+#header {
     margin-right: auto;
     margin-left: auto;
     width: 960px;
 }
 
+#page-title {
+  color: white;
+}
 
-.univ-header-bg .univ-header {
+#header-container #header {
     position: relative;
 }
 
-#page-title {
+#nav-bar-container {
+  width: 100%;
+  height: 45px;
+  background: rgba(30,30,30,0.8);
+  text-align: center;
+  padding-top: 3px;
+}
+
+#nav-bar {
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.nav-bar-link {
+  height: 40px;
+  margin: 0px;
+  margin-left: 4px;
+  padding: 8px;
+  padding-left: 35px;
+  padding-right: 35px;
+  color: #dcdcdc;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-decoration: none;
+  text-transform: uppercase;
+  cursor: auto; 
+}
+
+.nav-bar-link:hover {
+  background: #666666;
+  text-shadow: none;
   color: white;
+  text-decoration: none;
+}
+
+.nav-link-separator {
+    border-right: 1px solid white;
+    display: inline;
+    margin: 2px;
 }
 </style>
 
 <header class="page-row">
-  <div class="univ-header-bg">
-    <div class="univ-header">
+  <div id="header-container">
+    <div id="header">
       <h1 id="page-title">Header</h1>
     </div>
   </div>
 </header>
+
 <!-- NAVBAR -->
 <!-- data-spy="affix" data-offset-top="164" -->
 <div id="nav-bar-container" >
