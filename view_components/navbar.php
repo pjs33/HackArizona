2ce9e242354477f20d516190cee490fd59b63bcf
pjs33 <pjs33@email.arizona.edu>
2016-01-22 23:14:09 -0700

    Changed Navbar Look

--- view_components/navbar.php
+++ view_components/navbar.php
@@ -1,15 +1,66 @@
+<style type="text/css">
+#nav-bar {
+    margin-right: auto;
+    margin-left: auto;
+}
+
+.nav-bar-link {
+    color: #dcdcdc;
+    font-size: 16px;
+    line-height: 22px;
+    font-weight: 700;
+    letter-spacing: 1px;
+    text-decoration: none;
+    cursor: auto;
+    text-transform: uppercase;
+    height: 40px;
+    padding: 8px;
+    margin: 0px;
+    margin-left: 4px;
+    padding-left: 35px;
+    padding-right: 35px;
+}
+
+.nav-bar-link:hover {
+    background: #666666;
+    text-shadow: none;
+    color: white;
+    text-decoration: none;
+}
+
+#nav-bar-container {
+    width: 100%;
+    height: 45px;
+    background: rgba(30,30,30,0.8);
+    border-bottom: 5px solid #1a4ca3;
+    text-align: center;
+}
+
+.nav-link-separator {
+    border-right: 1px solid white;
+    display: inline;
+    margin: 2px;
+}
+</style>
+
+
 <!-- NAVBAR -->
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Lobbying Website!</a>
+<div id="nav-bar-container" data-spy="affix" data-offset-top="164">
+        <div id="nav-bar">
+            <div class="nav-link-separator"></div>
+            <div class="nav-link-separator"></div>
+            <a class="nav-bar-link btn primary-btn" href="#">Home</a>
+            <div class="nav-link-separator"></div>
+            <div class="nav-link-separator"></div>
+            <a class="nav-bar-link btn primary-btn" href="#">Subscribed Issues</a>
+            <div class="nav-link-separator"></div>
+            <div class="nav-link-separator"></div>
+            <a class="nav-bar-link btn primary-btn" href="#">Create Issue</a>
+            <div class="nav-link-separator"></div>
+            <div class="nav-link-separator"></div>
+            <a class="nav-bar-link btn primary-btn" href="#">Login</a>
+            <div class="nav-link-separator"></div>
+            <div class="nav-link-separator"></div>
+        </div>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li> 
-      <li><a href="#">Page 3</a></li> 
-    </ul>
-  </div>
-</nav>
 <!-- END NAVBAR -->
