946ea4afaf8940d67d2be1138e6cfa6a2495d871
pjs33 <pjs33@email.arizona.edu>
2016-01-22 22:52:09 -0700

    Put navbar into view_components folder

--- index.php
+++ index.php
@@ -24,23 +24,9 @@
 
   </head>
   <body>
-  
-  <!-- NAVBAR -->
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Lobbying Website!</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li> 
-      <li><a href="#">Page 3</a></li> 
-    </ul>
-  </div>
-</nav>
-<!-- END NAVBAR -->
-
+    <?php
+      require_once("view_components/navbar.php");
+    ?>
 
 
 <!-- START CAROUSEL -->
