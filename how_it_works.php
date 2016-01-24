05e08ab6f35ce3f5842d9425d55ba5cc4052162c
rubib <rubib@email.arizona.edu>
2016-01-23 18:31:35 -0700

    updated howitworks with text

--- how_it_works.php
+++ how_it_works.php
@@ -150,10 +150,18 @@
           <div class="textInfo">
             
               <div id="text-para"> 
-	              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	              The Lobbyist is a dynamic and exciting new venture dedicated to giving you, 
+	              as a citizen, a voice. Imagine if you could vote and choose topics relevant 
+	              to your local or even state community. It doesn't stop there however, 
+	              imagine if you could also choose and vote for country-wide reforms. The way Lobbyist works is simple:
+	              through voting and crowdfunding, particular issues can be addressed through Lobbying. Through crowdfunding,
+	              professional lobbyists can be hired to speak for you in Washington, rather than waiting for the political machine to address the issue
+	              in question (which could take years). The Lobbyist is meant to switch the power of legislation back to the people
+	              by reaffirming what is important to us as residents of the United States.
 	              <br>
 	              <br>
-	              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+	              Getting started: go ahead and vote on issues! If there is something you are particularly intrigued by
+	              or want to make an issue of they can also be submitted! Create an account to keep track of your submissions! Submit away!
 	           </div>
 	           
 	           <div id="picture">
