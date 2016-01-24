729c7ef37f3684ee3b400af27ce0331c8d361b79
mlewelling <Michael@dhcp-10-134-238-11.uawifi.arizona.edu>
2016-01-24 00:24:29 -0700

    Fixed FOOTBAR

--- view_components/footbar.php
+++ view_components/footbar.php
@@ -1,9 +1,10 @@
 <style>
 
-#container {
+#footerContainer {
 	border-bottom:7px solid #0c234b;
 	background: rgba(30,30,30,0.9);
 	height: 50px;
+
 }
 
 .footer-header {
@@ -11,7 +12,12 @@
 	margin:auto;
 	text-align: center;
 	padding-top:2px;
+	font-color: white;
+	text-align: center;
 }
+   
+    
+
 
 .footer-header a {
 	padding:10px;
@@ -26,7 +32,7 @@
 
 </style>
 
-	<div id="container">
+	<div id="footerContainer">
 		
 	       <div class="footer-header">
 			 		<a class="nav-bar-link btn primary-btn" href="http://localhost/HackArizona/help.php">Help</a>
