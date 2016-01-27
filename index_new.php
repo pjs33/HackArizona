567edba7179fbc1a38084f9578e8d6050a2e613d
mlewelling <mlewelling@email.arizona.edu>
2016-01-27 09:02:58 -0700

    Fixed test create / destroy

--- index_new.php
+++ index_new.php
@@ -8,10 +8,9 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="./img/favicon.png" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>The Lobbyists</title>
-
+    <link rel="shortcut icon" href="./img/favicon.png" />
     <!-- Bootstrap -->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/styles/indexstyles_new.css">
